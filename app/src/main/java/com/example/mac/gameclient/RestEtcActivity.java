48c5dc60eeb92e3b6518cb1d9d9d9fbb3b4cea01
Jeong kum ho <nashorn74@gmail.com>
2017-09-09 14:29:37 +0900

    RestEtcActivity 레이아웃 적용

--- app/src/main/java/com/example/mac/gameclient/RestEtcActivity.java
+++ app/src/main/java/com/example/mac/gameclient/RestEtcActivity.java
@@ -2,6 +2,7 @@ package com.example.mac.gameclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RestEtcActivity extends AppCompatActivity {
 
@@ -9,5 +10,33 @@ public class RestEtcActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rest_etc);
+    }
+
+    public void clickAddNoticeButton(View view) {
+
+    }
+
+    public void clickGetNoticeButton(View view) {
+
+    }
+
+    public void clickEditNoticeButton(View view) {
+
+    }
+
+    public void clickDeleteNoticeButton(View view) {
+
+    }
+
+    public void clickGetNoticeListButton(View view) {
+
+    }
+
+    public void clickAddVersionButton(View view) {
+
+    }
+
+    public void clickGetVersionButton(View view) {
+
     }
 }
